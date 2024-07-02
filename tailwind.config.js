853bb6976ef17659d4e84aef75167c88a61c0538
Duc Manh Nguyen <ndmanh3003@gmail.com>
2024-07-03 00:21:52 +0700

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme'
+
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    fontFamily: {
+      sans: ['Manrope', ...defaultTheme.fontFamily.sans],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
