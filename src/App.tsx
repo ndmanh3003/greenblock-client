d3c0f8bc89ecf442ae3b543731b81aded0fc2dbc
Duc Manh Nguyen <ndmanh3003@gmail.com>
2024-07-03 12:28:21 +0700

    component and cn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import { DatePicker } from 'antd';
+import InputC from './components/UI/InputC';
 
 function App() {
-  return <DatePicker className="m-5" />
+  return <>
+    <InputC className="m-5" />
+    <DatePicker className="m-5 rounded-full" />
+  </>
 }
 
 export default App;
---
+++ src/components/UI/InputC.tsx
@@ -0,0 +1,8 @@
+import { Input } from 'antd';
+import cn from '../../utils/cn';
+
+type IInput = React.ComponentProps<typeof Input>;
+
+export default function InputC({ className, ...props }: IInput) {
+  return <Input className={cn('rounded-full w-52 px-5 pb-1', className)} {...props} />
+}
