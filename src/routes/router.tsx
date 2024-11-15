835e9d1fc6d28443baf5ad2354a9b04cf3e39466
Duc Manh Nguyen <ndmanh3003@gmail.com>
2024-11-15 16:25:23 +0700

    fix(router): logout and error

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -10,43 +10,40 @@ import * as Pages from '@/pages'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route
-      element={<Pages.Layout />}
-      errorElement={<Pages.Error />}
-      path={Routes.HOMEPAGE}
-    >
-      <Route element={<Pages.HomePage />} path='/:id?' />
+    <Route errorElement={<Pages.Error />}>
+      <Route element={<Pages.Layout />} path={Routes.HOMEPAGE}>
+        <Route element={<Pages.HomePage />} path='/:id?' />
 
-      <Route element={<RetrictedPage />} path='/'>
-        <Route element={<Pages.LayoutAuth />} path='/'>
-          <Route element={<Pages.Login />} path={Routes.LOGIN} />
-          <Route element={<Pages.Register />} path={Routes.REGISTER} />
+        <Route element={<RetrictedPage />} path='/'>
+          <Route element={<Pages.LayoutAuth />} path='/'>
+            <Route element={<Pages.Login />} path={Routes.LOGIN} />
+            <Route element={<Pages.Register />} path={Routes.REGISTER} />
+          </Route>
         </Route>
-      </Route>
 
-      <Route element={<ProtectedPage />} path='/'>
-        <Route element={<Pages.LayoutWorkspace />} path='/'>
-          <Route
-            element={<Pages.Inspector />}
-            path={Routes.INSPECTOR + '/:id?'}
-          />
-          <Route element={<Pages.Business />} path={Routes.BUSINESS} />
-          <Route
-            element={<ProductList />}
-            path={Routes.BUSINESS + '/product/:id?'}
-          />
+        <Route element={<ProtectedPage />} path='/'>
+          <Route element={<Pages.LayoutWorkspace />} path='/'>
+            <Route
+              element={<Pages.Inspector />}
+              path={Routes.INSPECTOR + '/:id?'}
+            />
+            <Route element={<Pages.Business />} path={Routes.BUSINESS} />
+            <Route
+              element={<ProductList />}
+              path={Routes.BUSINESS + '/product/:id?'}
+            />
+          </Route>
         </Route>
+
+        <Route element={<Pages.Waitlist />} path={Routes.WAITLIST} />
+        <Route
+          element={<Pages.Error />}
+          path={import.meta.env.VITE_API_ERROR_PAGE}
+        />
+        <Route element={<Pages.Record />} path={Routes.RECORD} />
+        <Route element={<Pages.Admin />} path={Routes.ADMIN} />
+        <Route element={<Pages.Logout />} path={Routes.LOGOUT} />
       </Route>
-
-      <Route element={<Pages.Waitlist />} path={Routes.WAITLIST} />
-      <Route
-        element={<Pages.Error />}
-        path={import.meta.env.VITE_API_ERROR_PAGE}
-      />
-      <Route element={<Pages.Logout />} path={Routes.LOGOUT} />
-
-      <Route element={<Pages.Admin />} path={Routes.ADMIN} />
-      <Route element={<Pages.Record />} path={Routes.RECORD} />
     </Route>
   )
 )
